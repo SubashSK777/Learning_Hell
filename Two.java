08e32d5f07e4352bbc86e4aeddc21f367385f2d7
Subash Kumar <150102220+SubashSK777@users.noreply.github.com>
2024-09-23 09:00:41 -0700

    Two

---
+++ Two.java
@@ -0,0 +1,11 @@
+public class Two{
+    public static void main(String[] args) {
+        int time = 20;
+        if (time > 5) {
+            System.out.println("Good Evening");
+        } else {
+            System.out.println("Good Afternoon");
+        }
+
+    }
+}
