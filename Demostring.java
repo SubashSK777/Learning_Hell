2f16bac414dee827b157a615c32da69cd9abdf1c
Subash Kumar <150102220+SubashSK777@users.noreply.github.com>
2024-09-29 22:44:00 -0700

    Add files via upload

---
+++ Demostring.java
@@ -0,0 +1,13 @@
+public class Demostring{
+	public static void main(String[] args){
+		String greeting = "Hola";
+		String len = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		System.out.println(greeting);
+		System.out.println("The Length of the string is " + len.length());
+		System.out.println(len.toUpperCase());
+		System.out.println(len.toLowerCase());
+		System.out.println(len.indexOf("U"));
+		System.out.println(len.indexOf("V"));
+		
+	}
+}
