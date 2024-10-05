f6a6f3b9ced50aa47a6bf8c87dacf9bf4c7858ba
Subash <subashsk11831@gmail.com>
2024-10-05 13:39:10 +0530

    One

--- One.java
+++
@@ -1,9 +0,0 @@
-public class One {
-    public static void main(String[] args) {
-        if (20 > 18) {
-            System.out.println("20 is Big");
-        } else {
-            System.out.println("20 is Small");
-        }
-    }
-}
