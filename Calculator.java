5433b6ab0adaea8b8c48640cd6aafea63215f18d
Subash <subashsk11831@gmail.com>
2024-10-12 22:52:52 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the First Number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second Number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select an Operation");
+
     }
 }
