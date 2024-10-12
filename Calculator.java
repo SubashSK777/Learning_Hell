d11216bbcbbd3c9b1b661d1258fe1865c5f92941
Subash <subashsk11831@gmail.com>
2024-10-12 22:56:02 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,17 @@ public class Calculator {
         double num2 = scanner.nextDouble();
 
         System.out.println("Select an Operation");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = scanner.nextInt();
+        
+
+
+
+
 
     }
 }
