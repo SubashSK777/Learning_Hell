26ccd87b36cbc2901edf92db6b6abd2bcc8eb673
Subash <subashsk11831@gmail.com>
2024-10-13 21:39:12 +0530

    Update Random.java

--- Random.java
+++ Random.java
@@ -7,8 +7,17 @@ public class Random {
         
         Random random = new Random();
 
-        int RandomNumber = random.nextInt(100)+1;
-        
+        int randomNumber = random.nextInt(100)+1;
+
+        int attempts = 0;
+        int guess = 0;
+
+        while(guess != randomNumber){
+            System.out.println("Enter your Guess: ");
+            guess = scanner.nextInt();
+            
+
+        }
 
     }
 }
