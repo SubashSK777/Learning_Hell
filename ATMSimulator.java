5d02911f35926697ad36ba2efbf7b24457d3b45b
Subash <subashsk11831@gmail.com>
2024-10-14 18:13:17 +0530

    Update ATMSimulator.java

--- ATMSimulator.java
+++ ATMSimulator.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class ATMSimulator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
+
+        double balance = 1000;
+
+        
     }
     
 }
