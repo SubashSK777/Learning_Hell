5b0fe61587396586d9c199301e052586d30305ac
Subash <subashsk11831@gmail.com>
2024-10-14 18:23:34 +0530

    Update ATMSimulator.java

--- ATMSimulator.java
+++ ATMSimulator.java
@@ -6,7 +6,42 @@ public class ATMSimulator {
 
         double balance = 1000;
 
-        
+        int choice = 0;
+
+        while(choice !=4){
+
+            System.out.println("ATM Menu");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Exit");
+
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch(choice){
+
+                case 1:
+                    System.out.println("Your Balance is: $"+balance);
+                    break;
+
+                case 2:
+                    System.out.println("Deposit Selected");
+                    double deposit = 
+                    break;
+
+                case 3:
+                    System.out.println("Withdraw Selected");
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice. Please Try Again.");
+            }
+        }
     }
     
 }
