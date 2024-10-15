7ed8460d6d0e4eae4b9f604a3db6fe1bd72f6d8f
Subash <subashsk11831@gmail.com>
2024-10-15 18:01:40 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner scanner = new scanner(System.in);
-        
+        System.out.println("Enter a Word or Sentence: ");
+        String input = scanner.nextLine();
+
     }
 }
