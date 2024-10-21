96b5a82cf5ab07619f51db5ff5c2f223dea2141e
Subash <subashsk11831@gmail.com>
2024-10-21 21:53:48 +0530

    Update Slide1.java

--- Slide1.java
+++ Slide1.java
@@ -16,11 +16,19 @@ class Slide1 {
         maxSum = windowSum;
 
         for (int i = K; i < arr.length; i++){
-            /* So the sliding window is easy
-             * I must first add the next element of the array which is arr[i]
-             * i added it, now i must remove the last element
-             * what i did is 
-             */
+/* So the sliding window is easy
+* I must first add the next element of the array which is arr[i]
+* i added it, now i must remove the last element
+* what i did is subtract the current index of the element with the K
+* K is the maxWindow Limit
+* say now {1, 3, 5, 7, 9, 2, 4, 6, 8} is the the array i use. 
+* at 1st iteration: index 0,1,2 will be covered which is {1, 3, 5} = 9
+* at 2nd: index 3 will be added, which is 7. K max limit is 3; so i must remove 1 from the array
+* here arr[i] is the added element's index. and for the removeal we use arr[i - K],
+* which is the added index will subtract the maxLimit; 3-3; arr[i-K];
+* arr[i - K] is 0, then we will remove the element of 0th index now
+* 
+*/
             windowSum += arr[i] - arr[i - K];
 
         }
