3ef1eabd013a6cd78f4a44f3446d4e047626a9d7
Subash <subashsk11831@gmail.com>
2024-10-21 20:59:27 +0530

    Update Slide1.java

--- Slide1.java
+++ Slide1.java
@@ -1,8 +1,12 @@
 class Slide1 {
     public static void main(String[] args) {
         int []  arr = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+
         int K = 3;
         
+        int maxSum = 0;
+
+
     }
     
 }
