16ca87485efc1a1fff1aad65bcdf756125d1e7b1
Subash <subashsk11831@gmail.com>
2024-10-22 16:03:53 +0530

    Update Slide2.java

--- Slide2.java
+++ Slide2.java
@@ -11,6 +11,10 @@ public class Slide2 {
         int maxLength = 0; // To store the maximum length of 1s sequence
 
         for(int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+/*the for loop works like this for("variable"; for loop length; iterative action)
+* here i first defined what variable i will work on it is "windowEnd", so the windowEnd will start from its index 0
+* i defined it as int windowEnd = 0;. then for the length I defined window < arr.length. this will stop the loop at the 
+*length of the arr i have defined above. the arr index is 9. so it will loop 9 times.    */            
             if(arr[windowEnd] == 1){
                 maxOnesCount++;
             }
