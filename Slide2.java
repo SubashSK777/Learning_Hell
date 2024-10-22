ae3e7812d1f1be0830505895c9761117edaeac98
Subash <subashsk11831@gmail.com>
2024-10-22 14:36:24 +0530

    Add Slide2.java

---
+++ Slide2.java
@@ -0,0 +1,17 @@
+public class Slide2 {
+    public static void main(String[] args) {
+        int [] arr = {1, 0, 0, 0, 1, 1, 1, 0, 0, 1};
+
+        int K = 3;
+
+        int windowStart = 0; // Start of the sliding window
+
+        int maxOnesCount = 0; // To keep track of the number of 1s in the current window
+        
+        int maxLength = 0; // To store the maximum length of 1s sequence
+
+
+
+    }
+    
+}
