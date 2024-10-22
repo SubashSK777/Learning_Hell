2e254f0a18b121259c0c179e79ec03dcc2b57485
Subash <subashsk11831@gmail.com>
2024-10-22 16:40:10 +0530

    Update Slide2.java

--- Slide2.java
+++ Slide2.java
@@ -14,10 +14,19 @@ public class Slide2 {
 /*the for loop works like this for("variable"; for loop length; iterative action)
 * here i first defined what variable i will work on it is "windowEnd", so the windowEnd will start from its index 0
 * i defined it as int windowEnd = 0;. then for the length I defined window < arr.length. this will stop the loop at the 
-*length of the arr i have defined above. the arr index is 9. so it will loop 9 times.    */            
+*length of the arr i have defined above. the arr index is 9. so it will loop 9 times. and third the iterative operation.
+* it will add a specififc command to the variable i have brought in. it all works together like i enter the variable
+* then i will sset its limit for loop, and finally i will set the iterative value. it all works together and we get this     */            
             if(arr[windowEnd] == 1){
                 maxOnesCount++;
+            } if ((windowEnd - windowStart + 1) - maxOnesCount > K){
+                if(arr[windowStart] == 1){
+                    maxOnesCount --;
+                }
+                windowStart++;
+                maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
             }
+
 
         }
 
