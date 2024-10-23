ab1da39418c5f00cc8240768adbc3ad16e8f102b
Subash <subashsk11831@gmail.com>
2024-10-23 18:21:52 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Palindrome {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a Word or Sentence: ");
