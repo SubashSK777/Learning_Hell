013c72acd9a852609287a5e2ed1aa678ef4ceb55
Subash <subashsk11831@gmail.com>
2024-10-23 10:04:24 +0530

    Add Picasso.java

---
+++ Picasso.java
@@ -0,0 +1,10 @@
+public class Picasso {
+    public static void main(String[] args) {
+        int i = 0;
+
+        for(int i = 0; i < 100; i++){
+            System.out.println("I Love Java");
+        }
+    }
+    
+}
