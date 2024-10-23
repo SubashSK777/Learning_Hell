463c9f6dacc3bcab471c48f333e6bd8e5f35cca0
Subash <subashsk11831@gmail.com>
2024-10-23 10:05:02 +0530

    Update Picasso.java

--- Picasso.java
+++ Picasso.java
@@ -1,7 +1,5 @@
 public class Picasso {
     public static void main(String[] args) {
-        int i = 0;
-
         for(int i = 0; i < 100; i++){
             System.out.println("I Love Java");
         }
