c0ac885589b02744a1aeec139ecb3ad0a1ebeff9
Subash <subashsk11831@gmail.com>
2024-10-23 10:50:59 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    int fibo = scanner.nextInt();
+        int fibo = scanner.nextInt();
 
-    while(fibo < 0){
-        System.out.println("Enter a Non-Negative Number");
-        fibo = scanner.nextInt();
-    }
-     
-
-    int first = 0;
-
-    int second = 1;
-
-    for(int i = 0; i < fibo; i++){
-        System.out.println(first+" ");
-        int next = first + second;
-
-        first = second;
-
-        second = next;     
+        // Validate the input to ensure it's non-negative
+        while(fibo < 0){
+            System.out.println("Enter a Non-Negative Number");
+            fibo = scanner.nextInt();
         }
-        scanner.close();
-    }     
+
+        int first = 0;
+        int second = 1;
+
+        // Print the Fibonacci sequence up to the 'fibo' count
+        for(int i = 0; i < fibo; i++){
+            System.out.print(first + " ");  // Print on the same line
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        scanner.close();  // Close scanner
+    }
 }
