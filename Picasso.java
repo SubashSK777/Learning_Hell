211f48f6834a7e8bdd01c302e7073204bef0595f
Subash <subashsk11831@gmail.com>
2024-10-24 20:49:33 +0530

    Update Picasso.java

--- Picasso.java
+++ Picasso.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Picasso {
     
     public static void main(String[] args) {
-        for(int i = 1; i <= 25; i++){
-            System.out.println(i+". I Love Java");
-            
+        if(){
+            for(int i = 1; i <= 25; i++){
+                System.out.println(i+". I Love Java");  
+            }
         }
         System.out.println();
 
-        for(int j = 1; j <= 100; j+= 10){
-            System.out.println(j+". I Hate Java");
+        else(){
+            for(int j = 1; j <= 100; j+= 10){
+                System.out.println(j+". I Hate Java");
+            }
         }
     }
 }
