6fb5a5b43c5b78f9ec6d31572e23a971b586308b
Subash <subashsk11831@gmail.com>
2024-10-25 22:13:37 +0530

    Update LongSubarray.java

--- LongSubarray.java
+++ LongSubarray.java
@@ -2,7 +2,19 @@ public class LongSubarray {
     public static void main(String[] args) {
 
         int[] list = {2, 1, 5, 2, 3, 2};
-        
+
+        int S = 7;
+
+        int windowStart = 0;
+
+        int maxLength = 0;
+
+        int currentLength = 0;
+
+        for(int windowEnd = 0; windowEnd < list.length; windowEnd++){
+            
+        }
+
 
 
 
