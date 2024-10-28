a3e8411187e4cd24390fa62e14c6de6c179027fb
Subash <subashsk11831@gmail.com>
2024-10-28 21:20:59 +0530

    Update LongSubarray.java

--- LongSubarray.java
+++ LongSubarray.java
@@ -1,3 +1,14 @@
+/*
+* Given an array of positive integers and a positive integer S, 
+* find the length of the smallest contiguous subarray whose sum is greater than or equal to S. 
+* Return 0 if no such subarray exists.
+*
+* Example:
+* Input: list = [2, 1, 5, 2, 3, 2], S = 7
+* Output: 2
+* Explanation: The smallest subarray with a sum >= 7 is [5, 2] with length 2.
+*/
+
 public class LongSubarray {
     public static void main(String[] args) {
 
@@ -10,6 +21,10 @@ public class LongSubarray {
         int maxLength = 0;
 
         int windowSum = 0;
+
+        for(int windowEnd = 0; windowEnd < list.length; windowEnd++){
+
+        }
 
     }
 }
