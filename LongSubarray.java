a7cc209ea986981523bfadc7f2211eba94c734b4
Subash <subashsk11831@gmail.com>
2024-10-28 21:05:12 +0530

    Update LongSubarray.java

--- LongSubarray.java
+++ LongSubarray.java
@@ -11,14 +11,5 @@ public class LongSubarray {
 
         int windowSum = 0;
 
-        for(int windowEnd = 0; windowEnd < list.length; windowEnd++){
-            if (list[windowEnd] >= S){
-                maxLength++;
-            }
-        }
-
-
-
-
     }
 }
