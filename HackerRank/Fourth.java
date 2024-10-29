a688bc249f9e679b8a95c0b17ad203c01ab59d28
Subash <subashsk11831@gmail.com>
2024-10-29 12:47:56 +0530

    Add Fourth.java

---
+++ HackerRank/Fourth.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Fourth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String Str = scanner.nextLine();
+
+        System.out.println("String: " + Str);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + n);
+
+        scanner.close();
+
+    }
+
+}
