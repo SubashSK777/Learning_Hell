dc53eb4c6121b7e25e2efd8869158a51a5d8e9fd
Subash <subashsk11831@gmail.com>
2024-10-29 12:36:44 +0530

    Update Third.java

--- HackerRank/Third.java
+++ HackerRank/Third.java
@@ -7,9 +7,16 @@ public class Third {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         if(n >= 1 && n <= 100){
-            if ()
-
+            if (n % 2 != 0){
+                System.out.println("Weird");
+            } else if (n >= 2 && n <= 5){
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20){
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
         }
+        scanner.close();
     }
-    
 }
