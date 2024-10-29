b8a7562792c803416c5a3cc5e3a87dfa70222707
Subash <subashsk11831@gmail.com>
2024-10-29 12:03:51 +0530

    Update Second.java

--- HackerRank/Second.java
+++ HackerRank/Second.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Second {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
         // Complete this line
         // Complete this line
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         // Complete this line
         // Complete this line
     }
