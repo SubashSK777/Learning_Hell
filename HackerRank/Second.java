fd13a957214485dfea29a4581a4803357d4f35f1
Subash <subashsk11831@gmail.com>
2024-10-29 12:01:49 +0530

    Add Second.java

---
+++ HackerRank/Second.java
@@ -0,0 +1,18 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Second {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+    }
+}
