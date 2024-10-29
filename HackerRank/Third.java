94b9c9ea1648740cbc77598448b8d7c935b2c8b1
Subash <subashsk11831@gmail.com>
2024-10-29 12:27:18 +0530

    Update Third.java

--- HackerRank/Third.java
+++ HackerRank/Third.java
@@ -1,5 +1,15 @@
 package HackerRank;
 
+import java.util.Scanner;
+
 public class Third {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if(n >= 1 && n <= 100){
+            if ()
+
+        }
+    }
     
 }
