d0fcf95304b9249df1e2a32a67679a44260eeab7
Subash <subashsk11831@gmail.com>
2024-10-30 17:24:17 +0530

    Add Tables.java

---
+++ HackerRank/Tables.java
@@ -0,0 +1,17 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Tables {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for(int i = 1; i <= 10; i++){
+            int result = N * i;
+            System.out.println(N+" x " +i +" = "+ result);
+  
+        } 
+        scanner.close();
+    }
+    
+}
