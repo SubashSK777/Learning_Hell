d571f2335a03e96041f0949b5f6dc9bc62de3661
Subash <subashsk11831@gmail.com>
2024-10-30 18:02:40 +0530

    Add Calculator.java

---
+++ Basics/Calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter the First Number: ");
+            double num1 = scanner.nextDouble();
+            
+            System.out.println("Enter the second Number: ");
+            double num2 = scanner.nextDouble();
+            
+            System.out.println("Select an Operation");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            
+            int choice = scanner.nextInt();
+            
+            double result;
+            
+            switch(choice){
+                case 1 -> {
+                    result = num1 + num2;
+                    System.out.println("Result = "+result);
+                }
+                
+                case 2 -> {
+                    result = num2 - num2;
+                    System.out.println("Result = "+result);
+                }
+                
+                
+                case 3 -> {
+                    result = num1 * num2;
+                    System.out.println("Result = "+result);
+                }
+                
+                case 4 -> {
+                    if(num2 !=0){
+                        result = num1 / num2;
+                        System.out.println("Result = "+result);
+                    }  else {
+                        System.out.println("Error: Division by Zero");
+                    }
+                }
+                default -> System.out.println("Invalid Choice: Select a Valid Operation");
+            }
+        }
+    }
+}
