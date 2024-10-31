7df7c2331bcfd29a4001592a4a12903069c73c80
Subash <subashsk11831@gmail.com>
2024-10-31 18:28:09 +0530

    Add Parallelogram.java

---
+++ HackerRank/Parallelogram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Parallelogram {
+    public static void main(String[] args) {
+        int Height;
+        int Breadth;
+    }
+    public Parallelogram(int Height, int Breadth){
+        this.Height = Height;
+        this.Breadth = Breadth;
+    }
+}
