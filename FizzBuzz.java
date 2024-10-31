0464d069efdfb8f16f7750774df66d9f4712f299
Subash <subashsk11831@gmail.com>
2024-10-31 13:58:28 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,12 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-
+            String output = "";
+            if(i % 3 == 0) output += "Fizz";
+            if(i % 5 == 0) output += "Buzz";
+            if(i % 7 == 0) output += "Bazz";
+            if(i % 3 == 0 && i % 5 == 0) output += "FizzBuzz";
+            System.out.println(output.isEmpty()? i : output);
         }
     }
 }
