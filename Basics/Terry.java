6e3c903b9403e127565c2c1c0f03f3b25248c8f6
Subash <subashsk11831@gmail.com>
2024-11-01 20:13:03 +0530

    Add Terry.java

---
+++ Basics/Terry.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Terry {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+        String result = (n > 0) ? "Positive" : (n < 0) ? "Negative" : "This is 0";
+        System.out.println(result);
+        sc.close();
+    }
+}
