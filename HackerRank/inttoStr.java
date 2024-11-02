676e8fa1970339084f6788f2c641fcd1e3595a77
Subash <subashsk11831@gmail.com>
2024-11-02 16:50:17 +0530

    Add inttoStr.java

---
+++ HackerRank/inttoStr.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inttoStr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n >= 100 && n <= -100){
+            String s = Integer.toString(n);
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
+    }
+    
+}
