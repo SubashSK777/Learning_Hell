6ef3ad5b1eeda41379bb6dadd39dbc8480574281
Subash <subashsk11831@gmail.com>
2024-11-02 18:34:21 +0530

    Add Callander.java

---
+++ HackerRank/Callander.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Callander {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        int y = sc.nextInt();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(y, m, d);
+
+        
+    }
+}
