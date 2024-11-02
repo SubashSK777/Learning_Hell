34993f6108347454ddc2157742a489adf9bb04d6
Subash <subashsk11831@gmail.com>
2024-11-02 15:49:21 +0530

    Update ReReReeema.java

--- Basics/ReReReeema.java
+++ Basics/ReReReeema.java
@@ -2,7 +2,13 @@ public class ReReReeema {
     public static void main(String[] args) {
         
     }
-    public static void rem(){
-         
+    public static void rem(int n){
+         if(n==1){
+            System.out.println(1);
+         }
+         else{
+            System.out.println(n);
+            rem(n-1);
+         }
     }
 }
