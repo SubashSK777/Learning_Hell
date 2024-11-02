39c7788f125a9e8b38d5674c06615de5e9d820f4
Subash <subashsk11831@gmail.com>
2024-11-02 11:53:19 +0530

    Update BigE.java

--- Basics/BigE.java
+++ Basics/BigE.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
 public class BigE {
+    public static void main(String[] args) {
+        match("John", "Randy", "Shemus", "Big");
+    }
     
+    public static void match(String Pla1, String Pla2, String Pla3, String Pla4){
+        System.out.println(Pla1+ " & " +Pla2 + " vs " +Pla3 +" & "+ Pla4);
+
+    }
 }
