1e4ea6f38197d19eb37e26a8540a9fd4323283c7
Subash Kumar <subashsk11831@gmail.com>
2024-11-04 22:06:53 +0530

    rtyu

--- Java/HackerRank/inttoStr.java
+++ Java/HackerRank/inttoStr.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class inttoStr {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if(n >= 100 && n <= -100){
+            @SuppressWarnings("unused")
             String s = Integer.toString(n);
             System.out.println("Good job");
         } else {
