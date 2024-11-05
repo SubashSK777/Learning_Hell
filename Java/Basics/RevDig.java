63a211f5bb00b863ab052b167ea8056fce7c75d4
Subash Kumar <subashsk11831@gmail.com>
2024-11-05 22:09:31 +1100

    auto-commit

--- Java/Basics/RevDig.java
+++ Java/Basics/RevDig.java
@@ -5,7 +5,7 @@ public class RevDig{
     public static void Reverse(int n){
         int revDigit = 0;
         while(n > 0){
-            revDigit = revDigit * 10 
+            revDigit = revDigit * 10 +
         }
     }
 }
