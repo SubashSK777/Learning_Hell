ce1500c72b598fca42975d42faa48a2d99a08022
Subash Kumar <subashsk11831@gmail.com>
2024-11-06 20:29:23 +1100

    uh

---
+++ Java/Basics/Classy.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+
+class Classy{
+    String name;
+
+    LocalDate DoB; // year, month, day
+    
+    int age;
+
+    int Phnum;
+
+    String Location;
+
+    char Gender;
+
+    boolean Alive;
+
+    
+
+}
