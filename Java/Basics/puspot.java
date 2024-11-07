9295fd4cc66ce61cbe4838675d3b4fc3ce3d2abc
Subash Kumar <subashsk11831@gmail.com>
2024-11-07 15:55:05 +1100

    asdfgh

---
+++ Java/Basics/puspot.java
@@ -0,0 +1,10 @@
+public class puspot {
+    public static void main(String[] args) {
+        int l = 10;
+        int b = 12;
+        int h = 14;
+        int result = l*b*h;
+        System.out.printf("%.2f", (double)result);
+    }
+    
+}
--- Java/Basics/stringlen2.java
+++ Java/Basics/stringlen2.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
-public class stringlen2 {
+public class stringlen2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int length = input.length();
+        String word = sc.nextLine();
+        int length = word.length();
         System.out.println(length);
-        sc.close();
     }
 }
