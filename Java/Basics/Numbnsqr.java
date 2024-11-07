234d0d38a6d8868a0685c7807170e4dab8ca2d93
Subash Kumar <subashsk11831@gmail.com>
2024-11-07 19:46:55 +1100

    fgh

---
+++ Java/Basics/Numbnsqr.java
@@ -0,0 +1,10 @@
+public class Numbnsqr {
+    public static void main(String[] args) {
+        int n = 500;
+        int i = 0;
+        while (i <= n) {
+            System.out.println("Number: "+i+" & the Sqr Root: "+i*i);
+            i++;
+        }
+    }
+}
