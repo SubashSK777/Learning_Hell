64c8bcc48ad2da3ee180017ba71b850a576c8417
Subash Kumar <subashsk11831@gmail.com>
2024-11-08 21:55:45 +0530

    2024-11-08T16:25:45.107Z:

---
+++ Java/LC/MergeStrAlt.java
@@ -0,0 +1,11 @@
+class Append {
+    public static void mergeAlternately(String word1, String word2) {
+
+    }
+}
+
+public class MergeStrAlt{
+    public static void main(String[] args) {
+        Append.mergeAlternately("Subash", "Kumar");
+    }
+}
