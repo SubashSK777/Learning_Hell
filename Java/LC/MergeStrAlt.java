74ad4dbd4c7a0668fff6efaea5d3f32e15ad7255
Subash Kumar <subashsk11831@gmail.com>
2024-11-09 22:02:22 +0530

    2024-11-09T16:32:22.161Z:

--- Java/LC/MergeStrAlt.java
+++ Java/LC/MergeStrAlt.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class MergeStrAlt{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.nextLine();
+        String word
         for (int i = 0, j = 0; i < word1.length() || j < word2.length(); i++, j++){
             char c = i < word1.length() ? word1.charAt(i) : ' ';
             char d = j < word2.length() ? word2.charAt(j) : ' ';
             System.out.print(c + " " + d + " ");
+        }
     }
 }
-
 
