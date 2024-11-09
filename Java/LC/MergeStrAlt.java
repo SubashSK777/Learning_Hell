70fef62ca7159c41dac4cd9c968ee28fd5df1f04
Subash Kumar <subashsk11831@gmail.com>
2024-11-09 22:01:18 +0530

    2024-11-09T16:31:18.351Z:

--- Java/LC/MergeStrAlt.java
+++ Java/LC/MergeStrAlt.java
@@ -1,16 +1,9 @@
-class Solution {
-    public static void mergeAlternately(String word1, String word2) {
+public class MergeStrAlt{
+    public static void main(String[] args) {
         for (int i = 0, j = 0; i < word1.length() || j < word2.length(); i++, j++){
             char c = i < word1.length() ? word1.charAt(i) : ' ';
             char d = j < word2.length() ? word2.charAt(j) : ' ';
             System.out.print(c + " " + d + " ");
-        }
-    }
-}
-
-public class MergeStrAlt{
-    public static void main(String[] args) {
-        Solution.mergeAlternately("Subash", "Kumar");
     }
 }
 
