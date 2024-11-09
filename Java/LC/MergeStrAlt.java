020456c634296ee6974a987d26870df73dce07bb
Subash Kumar <subashsk11831@gmail.com>
2024-11-09 20:41:52 +0530

    2024-11-09T15:11:52.831Z:

--- Java/LC/MergeStrAlt.java
+++ Java/LC/MergeStrAlt.java
@@ -3,7 +3,9 @@ class Append {
         String word1;
         String word2;
         for (int i = 0, j = 0; i < word1.length() || j < word2.length(); i++, j++){
-            char c = i < word1.length() ? word1.charAt(i)
+            char c = i < word1.length() ? word1.charAt(i) : ' ';
+            char d = j < word2.length() ? word2.charAt(j) : ' ';
+
         }
 
     }
