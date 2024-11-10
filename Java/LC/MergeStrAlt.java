a1983bf557a9618028341862c7bd7c3c91b28261
Subash Kumar <subashsk11831@gmail.com>
2024-11-10 20:15:16 +0530

    2024-11-10T14:45:16.586Z:

--- Java/LC/MergeStrAlt.java
+++ Java/LC/MergeStrAlt.java
@@ -1,7 +1,9 @@
 public class MergeStrAlt {
     public String mergeAlternately(String word1, String word2) {
         String result = "";
-        for(int i = 0)
+        for(int i = 0, j = 0; i < word1.length() || j < word2.length(); i++, j++){
+            
+        }
 
 
 
