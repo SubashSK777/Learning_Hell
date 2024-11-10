d21f6eb230ae6472912903b604920193f8ed7d30
Subash Kumar <subashsk11831@gmail.com>
2024-11-10 21:10:36 +0530

    2024-11-10T15:40:36.149Z:

--- Java/Basics/Alpha.java
+++ Java/Basics/Alpha.java
@@ -1,6 +1,7 @@
 public class Alpha {
     public static void main(String[] args) {
-        for (char a = 'A'; a <= 'Z'; a++)
+        for (char a = 'A'; a <= 'z'; a++){
+            System.out.print(a + " ");
+        }
     }
-    
 }
