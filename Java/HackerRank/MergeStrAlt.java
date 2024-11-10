d01094b845af3be0e37a4af6fea4c3fa0e8e3af7
Subash Kumar <subashsk11831@gmail.com>
2024-11-10 20:12:07 +0530

    2024-11-10T14:42:06.977Z:

---
+++ Java/HackerRank/MergeStrAlt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MergeStrAlt{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.nextLine();
+        String word2 = sc.nextLine();
+        for (int i = 0, j = 0; i < word1.length() || j < word2.length(); i++, j++){
+            char c = i < word1.length() ? word1.charAt(i) : ' ';
+            char d = j < word2.length() ? word2.charAt(j) : ' ';
+            System.out.print(c + " " + d + " ");
+        }
+        sc.close();
+    }
+}
+
