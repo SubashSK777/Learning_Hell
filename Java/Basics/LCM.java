f6a7a5fb92cd23c90ccc2a6bf1e8704c667ce286
Subash Kumar <subashsk11831@gmail.com>
2024-11-12 20:54:55 +1100

    2024-11-12T09:54:55.186Z:

--- Java/Basics/LCM.java
+++ Java/Basics/LCM.java
@@ -1,5 +1,6 @@
 public class LCM {
     public static void main(String[] args) {
+        System.out.println();
         
     }
     public static int gcd(int a, int b){
@@ -12,6 +13,5 @@ public class LCM {
     }
     public static int lcm(int a, int b){
         return (a * b) / gcd(a, b);
-
     }
 }
