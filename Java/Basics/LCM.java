51219c18774d6f22eec7308f4243ec1c6a0b215f
Subash Kumar <subashsk11831@gmail.com>
2024-11-12 20:55:59 +1100

    2024-11-12T09:55:59.284Z:

--- Java/Basics/LCM.java
+++ Java/Basics/LCM.java
@@ -1,6 +1,6 @@
 public class LCM {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(lcm(18, 3));
         
     }
     public static int gcd(int a, int b){
