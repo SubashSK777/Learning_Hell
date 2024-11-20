ad23bccce9b58d4710463232f96726aa5d2db398
Subash Kumar <subashsk11831@gmail.com>
2024-11-21 06:16:18 +1100

    2024-11-20T19:16:18.172Z:

--- C/Factty.c
+++ C/Factty.c
@@ -4,12 +4,12 @@ int main(){
   int num;
   printf("Enter a Number: ");
   scanf("%d", &num);
-  int beg = 1;
+  unsigned long long int beg = 1;
   int i = 1;
   while (i <= num) {
     beg *= i;
     i++; 
   }
-  printf("%d", beg);
+  printf("%llu", beg);
   return 0;
 }
