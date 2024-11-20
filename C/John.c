0b353855cd95c7d32bbae2774e83fefaa08063a1
Subash Kumar <subashsk11831@gmail.com>
2024-11-21 00:12:56 +1100

    2024-11-20T13:12:55.908Z:

--- C/John.c
+++ C/John.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main () {
   char name[] = "John Cena";
@@ -6,10 +7,12 @@ int main () {
   float weight = 80.9;
   char my = 'C';
   bool stat = true;
+  unsigned long int big = -129;
 
-  printf("I am %s and my Age is %d, guess what my weight is, it's only %f\n", name, age, weight);
-  printf("You can't %c meeeee.", my);
-  printf
+  // printf("I am %s and my Age is %d, guess what my weight is, it's only %f\n", name, age, weight);
+  // printf("You can't %c meeeee.\n", my);
+  // printf("Is John Cena Alive ??: %d", stat);
+  printf("%lu", big);
 
   return 0;
 }
