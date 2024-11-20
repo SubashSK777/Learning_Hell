99bd351b7658e49357825b8426df0cf415a1fceb
Subash Kumar <subashsk11831@gmail.com>
2024-11-21 00:02:52 +1100

    2024-11-20T13:02:52.386Z:

--- C/John.c
+++ C/John.c
@@ -5,9 +5,11 @@ int main () {
   int age = 40;
   float weight = 80.9;
   char my = 'C';
+  bool stat = true;
 
-  printf("I am %s and my Age is %d, guess what my weight is, it's only %f", name, age, weight);
-  
+  printf("I am %s and my Age is %d, guess what my weight is, it's only %f\n", name, age, weight);
+  printf("You can't %c meeeee.", my);
+  printf
 
   return 0;
 }
