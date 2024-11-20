96cb9d5af6ed5185b810e779bfe046d799c3bbf9
Subash Kumar <subashsk11831@gmail.com>
2024-11-21 00:00:47 +1100

    2024-11-20T13:00:47.607Z:

---
+++ C/John.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+  char name[] = "John Cena";
+  int age = 40;
+  float weight = 80.9;
+  char my = 'C';
+
+  printf("I am %s and my Age is %d, guess what my weight is, it's only %f", name, age, weight);
+  
+
+  return 0;
+}
