b15fe142d6d7b59024e5f6fafff7b7baf11e0c75
Subash Kumar <subashsk11831@gmail.com>
2024-11-21 06:25:23 +1100

    sdfghjk

--- C/datatypes.c
+++ C/datatypes.c
@@ -1,1 +1,15 @@
-#inc
+#include <stdio.h>
+
+int main() {
+  int item;
+  printf("Enter the Number of Items: ");
+  scanf("%d", &item);
+  float cost_of_item;
+  printf("Enter the Cost of the Item: ");
+  scanf("%f", &cost_of_item);
+  float tots_cost = item * cost_of_item;
+
+  printf("You have brought %d Items for the Cost of $ %.2f per item, Total Cost is %.2f",item, cost_of_item, tots_cost);
+
+  return 0;
+}
