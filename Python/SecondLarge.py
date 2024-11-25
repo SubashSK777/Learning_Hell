12a4b7087b16af81a7f70a0bfa509cd910beeb80
Subash Kumar <subashsk11831@gmail.com>
2024-11-26 03:06:40 +1100

    2024-11-25T16:06:40.679Z:

--- Python/SecondLarge.py
+++ Python/SecondLarge.py
@@ -3,4 +3,9 @@ def print2ndlargest(self, arr):
   if n < 2:
     return -1
   
-  first = second = float()
+  first = second = float('-inf')
+  for num in arr:
+    if num > first:
+      second = first
+      first = num
+    elif num > second and num
