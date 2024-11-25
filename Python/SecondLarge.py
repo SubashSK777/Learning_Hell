ff4873e1a8e4d85dfad49f0cf9f7e437553cc6b6
Subash Kumar <subashsk11831@gmail.com>
2024-11-26 03:07:44 +1100

    2024-11-25T16:07:44.395Z:

--- Python/SecondLarge.py
+++ Python/SecondLarge.py
@@ -8,4 +8,9 @@ def print2ndlargest(self, arr):
     if num > first:
       second = first
       first = num
-    elif num > second and num
+    elif num > second and num != first:
+      second = num
+      
+  for second == float('-inf'):
+    return -1
+       
