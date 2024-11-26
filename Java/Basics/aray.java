dccd9f80344eddbab87000660ce6664e8d86a728
Subash Kumar <subashsk11831@gmail.com>
2024-11-27 00:22:54 +0530

    2024-11-26T18:52:54.664Z:

--- Java/Basics/aray.java
+++ Java/Basics/aray.java
@@ -1,28 +1,28 @@
 public class aray {
   public static void main (String [] args) {
-    int num[] = { 1, 3, 5, 7, 9 };
-    for (int n : num) {
-      System.out.println(n); 
-    }
+    // int num[] = { 1, 3, 5, 7, 9 };
+    // for (int n : num) {
+    //   System.out.println(n); 
+    // }
 
-    String Name[] = { "Subash", "Bash", "Sethu", "Pandi" };
-    for (String name : Name) {
-      System.out.println(name);
-    }
+    // String Name[] = { "Subash", "Bash", "Sethu", "Pandi" };
+    // for (String name : Name) {
+    //   System.out.println(name);
+    // }
+
+    // double CGPA[] = { 3.77, 1.43, 9.43, 3.69, 8.54 };
+    // for (double c : CGPA) {
+    //   System.out.println(c);
+    // }
+
+    // float GPA[] = { 3.77f, 1.43f, 9.43f, 3.69f, 8.54f };
+    // for (float d : GPA) {
+    //   System.out.println(d);
+    // }
 
     double CGPA[] = { 3.77, 1.43, 9.43, 3.69, 8.54 };
     for (double c : CGPA) {
-      System.out.println(c);
-    }
-
-    float GPA[] = { 3.77f, 1.43f, 9.43f, 3.69f, 8.54f };
-    for (float d : GPA) {
-      System.out.println(d);
-    }
-
-    double CGPA[] = { 3.77, 1.43, 9.43, 3.69, 8.54 };
-    for (double c : CGPA) {
-      System.out.println(c);
+      System.out.println(Double.toString(c));
     }
   }
 }
