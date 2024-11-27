f49d79ae086479c01d5c97404eaa5c6b380bcbcd
Subash Kumar <subashsk11831@gmail.com>
2024-11-27 07:18:23 +0530

    2024-11-27T01:48:22.976Z:

--- Java/Basics/sortrring.java
+++ Java/Basics/sortrring.java
@@ -2,7 +2,7 @@ public class sortrring {
   public static void main (String [] args) {
     char[] lett = { 'h', 'u', 'w', 't', 'a', 'k', 'l', 'v'};
 
-    // int length = char.length();
+    int length = lett.length;
 
     for (char you : lett) {
       System.out.println(you);
