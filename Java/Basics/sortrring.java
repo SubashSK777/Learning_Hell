e6db1b00875d88773f5275f9854b3d557f2e359f
Subash Kumar <subashsk11831@gmail.com>
2024-11-28 15:24:59 +0530

    2024-11-28T09:54:59.807Z:

--- Java/Basics/sortrring.java
+++ Java/Basics/sortrring.java
@@ -1,11 +1,11 @@
-import java.util.
+import java.util.Arrays;
 
 public class sortrring {
   public static void main (String [] args) {
     char[] lett = { 'h', 'u', 'w', 't', 'a', 'k', 'l', 'v'};
 
     int length = lett.length;
-    Array.sort(lett);
+    Arrays.sort(lett);
 
     for (char you : lett) {
       System.out.println(you);
