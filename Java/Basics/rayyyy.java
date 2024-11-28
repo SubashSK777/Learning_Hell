871721beef1424fe90b17fb6b7e03f4d778a8624
Subash Kumar <subashsk11831@gmail.com>
2024-11-28 15:45:48 +0530

    2024-11-28T10:15:48.697Z:

--- Java/Basics/rayyyy.java
+++ Java/Basics/rayyyy.java
@@ -1,8 +1,8 @@
 public class rayyyy {
   public static void main (String [] args) {
-    int[] tabel = new int[5];
-    tabel[2] = 7;
-    tabel[4] = 9;
+    String[] tabel = new int[5];
+    tabel[2] = "Walk";
+    tabel[4] = "Jump";
     for (int i = 0; i < 5; i++) {
       System.out.println(tabel[i]);
     }
