066f78fbf33483bd81b44ae7b9e94521d4e976e1
Subash Kumar <subashsk11831@gmail.com>
2024-11-28 16:00:19 +0530

    2024-11-28T10:30:18.972Z:

--- Java/Basics/2drayyy.java
+++ Java/Basics/2drayyy.java
@@ -1,6 +1,10 @@
 public class 2drayyy {
   public static void main (String [] args) {
     int[][] matrix = new int[5][3];
-    matrix
+    matrix[0][0] = 5;
+    matrix[2][2] = 8;
+    matrix[4][2] = 10;
+
+    for(int i = 0; )
   }
 }
