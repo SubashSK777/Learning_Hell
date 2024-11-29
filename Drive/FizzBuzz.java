9e933af10d854feaad4a342f2fec844ceede7754
Subash Kumar <subashsk11831@gmail.com>
2024-11-30 04:01:44 +0530

    2024-11-29T22:31:44.568Z:

--- Drive/FizzBuzz.java
+++ Drive/FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz
   public static void main (String [] args)
   {
     int n = 15;
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
       System.out.println(i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i;
     }
