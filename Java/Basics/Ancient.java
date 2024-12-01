c0d3fe16abacc5d5fbf21b27da59b842ee3cd0de
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 16:18:33 +0530

    2024-12-01T10:48:33.766Z:

--- Java/Basics/Ancient.java
+++ Java/Basics/Ancient.java
@@ -2,6 +2,8 @@ public class Ancient {
   int x = 5;
   public static void main (String [] args) {
     Ancient num = new Ancient();
+    Ancient num1 = new Ancient();
+    System.out.println(num1.x + 1);
     System.out.println(num.x);
   }
 }
