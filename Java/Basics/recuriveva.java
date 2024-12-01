65f8a704afb6001a3c9fa270c2e97d458935b301
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 10:27:45 +0530

    2024-12-01T04:57:45.155Z:

--- Java/Basics/recuriveva.java
+++ Java/Basics/recuriveva.java
@@ -1,13 +1,15 @@
 public class recuriveva {
   public static void main(String[] args) {
     NattyNum(1);
+    System.out.println();
+    NattyNum(12);
     
   }
   public static void NattyNum(int n) {
     if (n == 15) {
-      System.out.println(15);
+      System.out.print(15);
     } else {
-      System.out.println(n);
+      System.out.print(n + " ");
       NattyNum( n + 1);
 
     }
