8e8f4133b0c6a9381e2dd8a7ef5a78ee92935089
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 08:45:30 +0530

    2024-12-01T03:15:30.289Z:

--- Java/Basics/aray2.java
+++ Java/Basics/aray2.java
@@ -2,7 +2,7 @@ public class aray2
 {
   public static void main(String[] args) 
   {
-    int [] [] [] aray2d = {{2, 4, 6, 8, 0}, {3, 5 ,7, 9}, {1, 3, 5, 7, 8}};
+    int [] [] [] aray2d = {{{2, 4, 6, 8, 0}}, {{3, 5 ,7, 9}}, {{1, 3, 5, 7, 8}}{};
 
     for (int i = 0; i < aray2d.length; i++) 
     {
