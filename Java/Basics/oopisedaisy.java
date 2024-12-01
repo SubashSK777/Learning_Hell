3dee795112d350d9d518b5b316fecb5e0bda5f1c
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 08:50:46 +0530

    2024-12-01T03:20:46.789Z:

---
+++ Java/Basics/oopisedaisy.java
@@ -0,0 +1,11 @@
+public class oopisedaisy {
+  public static void main(String[] args) {
+    printer();
+    printer();
+    printer();
+  }
+
+  public static void printer() {
+    System.out.println("My First OOPsieeee");
+  }
+}
