6bd10d6acb31e9d2b386e1adaabb583ae3d78d4c
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 09:02:00 +0530

    2024-12-01T03:32:00.375Z:

--- Java/Basics/oopsieAge.java
+++ Java/Basics/oopsieAge.java
@@ -1,11 +1,12 @@
 public class oopsieAge {
   public static void main(String[] args) {
-    AgeChecker(20);
-    AgeChecker(0);
+    AgeChecker(17);
+    AgeChecker(18);
+    AgeChecker(19);
   }
   
   public static void AgeChecker (int age) {
-    if (age < 18) {
+    if (age <= 18) {
       System.out.println("Get Lost Kid !!");
     } else {
       System.out.println("Enjoy Your Drinks Man !!!");
