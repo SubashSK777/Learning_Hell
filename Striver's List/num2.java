327ff879b8fd7fe1e7b6604301225204d4699554
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 06:34:32 +0530

    2024-12-01T01:04:32.859Z:

--- Striver's List/num2.java
+++ Striver's List/num2.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class num2 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number: ");
+    int num1 = sc.nextInt();
+
     
   }
 }
