ec8d973f1e00b63e34403182c25f8c46515270cf
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 12:19:01 +0530

    2024-12-01T06:49:01.620Z:

--- Java/Basics/Ribaraibabri.java
+++ Java/Basics/Ribaraibabri.java
@@ -2,6 +2,8 @@ public class Ribaraibabri {
   public static void main(String[] args) {
     
   }
-  public 
+  public static void shortre (int n) {
+    System.out.println((if n == 15) ? 15 : );
+  }
   
 }
