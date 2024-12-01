8ee0a74d264341e6b4345ae30784283f33f8b1b8
Subash Kumar <subashsk11831@gmail.com>
2024-12-01 09:09:19 +0530

    2024-12-01T03:39:19.907Z:

--- Java/Basics/oopsmaoverload.java
+++ Java/Basics/oopsmaoverload.java
@@ -4,6 +4,10 @@ public class oopsmaoverload {
   }
   
   public static void AddNum (int a, int b) {
-    System.out.println(Sum of);
+    System.out.println("Sum of 2 Int is " + (a + b));
+  }
+
+  public static void AddNum (float a, float b) {
+    System.out.println("Sum of 2 float is " + ());
   }
 }
