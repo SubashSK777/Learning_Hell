74c82714b78e3bb127520a412c2381d7f0753f04
Subash Kumar <subashsk11831@gmail.com>
2024-12-02 07:34:59 +0530

    2024-12-02T02:04:59.484Z:

--- Python/Stringa.py
+++ Python/Stringa.py
@@ -1,11 +1,7 @@
-a = """Chuck Norris sleeps with a pillow under his gun. Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling. Chuck Norris plays Jenga with Stonehenge. When Chuck Norris does a pushup, he’s pushing the Earth down. When Chuck Norris writes, he makes paper bleed.
+x = "banana"
 
-Chuck Norris once had a heart attack. His heart lost. Chuck Norris used to beat the shit out of his shadow because it was following to close. It now stands a safe 30 feet behind him. Chuck Norris narrates Morgan Freeman’s life. Chuck Norris can hear sign language. When God said, “Let there be light!” Chuck said, “Say Please.”.
+i = len(x) - 1
 
-Bigfoot claims he once saw Chuck Norris. Chuck Norris named his daughter Mercy. The day she was born was the only day Chuck Norris ever had Mercy. Chuck Norris sleeps with a pillow under his gun. Chuck Norris plays Jenga with Stonehenge. Chuck Norris is the reason why Waldo is hiding.
-
-Chuck Norris has a mug of nails instead of coffee in the morning. Chuck Norris is the reason why Waldo is hiding. A bulletproof vest wears Chuck Norris for protection. If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win. Chuck Norris doesn’t wear a watch. He decides what time it is.
-
-"""
-
-print(a)
+while i >= 0:
+	print(x[i])
+    i -= 1
