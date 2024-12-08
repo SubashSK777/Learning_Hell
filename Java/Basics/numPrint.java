5b51bbb8b9322cc49c12696e2273217773ea614d
Subash Kumar <subashsk11831@gmail.com>
2024-12-08 07:30:08 +0530

    fgh

--- Java/Basics/numPrint.java
+++ Java/Basics/numPrint.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class numPrint {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a Number: ");
+    System.out.print("Enter a Number: ");
     int n =sc.nextInt();
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
       System.out.println(i);
     }
 
