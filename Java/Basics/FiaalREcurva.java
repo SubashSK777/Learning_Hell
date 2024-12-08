c6f278db9d51e9ba13b0eeebf791aa4f92d4f18a
Subash Kumar <subashsk11831@gmail.com>
2024-12-08 07:26:07 +0530

    2024-12-08T01:56:07.528Z:

--- Java/Basics/FiaalREcurva.java
+++ Java/Basics/FiaalREcurva.java
@@ -2,13 +2,13 @@ public class FiaalREcurva {
 
   public static int Recur(int current, int end) {
 
+    System.out.println(current);
     if (current == end) {
       return current;
-      System.out.println(current);
+
     } else {
-      return current + Recur( current + 1, end);
-      System.out.println(current);
-    }
+      return 
+    }current + Recur( current + 1, end);
   }
   
   public static void main(String[] args) {
