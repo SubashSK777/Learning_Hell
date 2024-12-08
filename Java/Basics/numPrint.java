ef2ab02aea6daf90ec73cc7f1372347df7f14067
Subash Kumar <subashsk11831@gmail.com>
2024-12-08 07:28:12 +0530

    2024-12-08T01:58:12.708Z:

--- Java/Basics/numPrint.java
+++ Java/Basics/numPrint.java
@@ -4,6 +4,9 @@ public class numPrint {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a Number: ");
+    int n =sc.nextInt();
+
     
+
   }
 }
