e46bee9ec22876f73ce2650a24ffce41a5ce053a
Subash Kumar <subashsk11831@gmail.com>
2024-12-08 11:22:19 +0530

    2024-12-08T05:52:19.213Z:

--- Java/Basics/nunm.java
+++ Java/Basics/nunm.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class nunm {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
     System.out.println("Enter a Number: ");
+    int n = sc.nextInt();
 
     int i = 0;
 
