a5401dc42358b7bd4e091dd29121258b6c401e1a
SubashSK777 <subashsk11831@gmail.com>
2025-06-17 15:07:28 +0530

    uyggik

---
+++ Java/Basics/Ronaldo_Number.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Ronaldo_Number{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+
+        System.out.println(n.charAt(0));
+        
+    }
+}
